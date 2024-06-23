c7b0694e3d60c9339e515a1c2aa87ed862803a8c
Josha Ho <joshaho@Joshas-Mac-mini.local>
2024-06-23 18:04:35 -0400

    fix DB Line

--- school/settings.py
+++ school/settings.py
@@ -239,7 +239,7 @@ if DEVELOPMENT_MODE is True:
             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
         }
     }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+elif len(sys.argv) > 0:
     if os.getenv("DATABASE_URL", None) is None:
         raise Exception("DATABASE_URL environment variable not defined")
     DATABASES = {
